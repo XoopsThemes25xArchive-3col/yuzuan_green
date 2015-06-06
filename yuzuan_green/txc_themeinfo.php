49d18de9dc9467e6334b4bc5df953108a6781fb1
mambax7 <mambax7@gmail.com>
2015-06-06 10:06:58 -0400

    Original files

---
+++ yuzuan_green/txc_themeinfo.php
@@ -0,0 +1,7 @@
+<?php
+	$txc_themeinfo["name"] = "yuzuan_green";
+	$txc_themeinfo["author"] = "¹ÂÓêËæ·ç";
+	$txc_themeinfo["author_url"] = "http://www.f-wind.cn/";
+	$txc_themeinfo["detail_url"] = "http://xoops.org.cn/modules/newbb/viewtopic.php?topic_id=11558";
+	$txc_themeinfo["download_url"] = "http://xoops.org.cn/modules/newbb/viewtopic.php?topic_id=11558";
+?>
